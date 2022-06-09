eb2761f849afb4aa9a45eb5a5ddbe363755b821f
Ken Lenhart <omnipaid@gmail.com>
2022-06-09 11:19:37 -0700

    Update define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,31 @@
 
 # YOU DO the rest:
 
+# The include? method is called on the string "Hello World" with the argument "Hello".
+# The include? method executes and checks if the string "Hello World" includes the string "Hello"
+# and implicitly returns TRUE.
 
 "Hello World".include?("Hello")
 
+# The end_with? method is called on the string "Hello World" with the argument "Hello".
+# The end_with? method executes and checks if the string "Hello World" ends with the string "Hello"
+# and returns FALSE.
 
 "Hello World".end_with?("Hello")
+
+# The end_with? method is called on the string "Hello World" with the argument "rld".
+# The end_with? method executes and checks if the string "Hello World" ends with "rld"
+# and implicitly returns TRUE
 
 
 "Hello World".end_with?("rld")
 
+# The even? method is called on the integer 12. The even? method executes and checks if
+# 12 is an even number and implicitly returns TRUE.
 
 12.even?
 
+# The next method is called on the integer 19. The next method executes and increases the
+# value of 18 to 19 and implicitly returns 19.
 
 18.next
-
-
